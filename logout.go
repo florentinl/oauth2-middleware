90ae06f5fb09cbca43b1710ee52658287a35ea93
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-07-21 16:24:46 +0200

    Allow customization of logout route

--- logout.go
+++ logout.go
@@ -7,8 +7,9 @@ import (
 
 func (config Oauth2Config) logout(w http.ResponseWriter, r *http.Request) {
 	clearUserCookie(w)
+	redirectLogout := r.FormValue("redirect_logout")
 	parameters := url.Values{
-		"redirect_logout": {config.BaseUri + "/"},
+		"redirect_logout": {redirectLogout},
 	}
 	http.Redirect(w, r, config.LogoutUri+"?"+parameters.Encode(), 302)
 }
