a742ab749db82ed987e286d2151dcf7ad17cc6a2
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-07-21 16:40:00 +0200

    Configure REDIS_PASSWORD as env var

--- env.template.sh
+++ env.template.sh
@@ -2,3 +2,6 @@ export CLIENT_ID=clientid
 export CLIENT_SECRET=clientsecret
 export BASE_URI=http://localhost:8080
 export SECRET=verylongandhardtoguess
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
+export REDIS_PASSWORD=
--- oauth2-middleware.go
+++ oauth2-middleware.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
-		Password: "",
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 
