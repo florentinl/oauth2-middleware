8d6f533c04200971645c64327352cdfd98b3591c
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-07-23 10:34:02 +0200

    prevent authorization bypass on path /_health

--- oauth2-middleware.go
+++ oauth2-middleware.go
@@ -13,6 +13,10 @@ import (
 
 func substituteXHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/_health" {
+			next.ServeHTTP(w, r)
+			return
+		}
 		if r.Header.Get("X-Forwarded-Host") != "" {
 			r.URL.Host = r.Header.Get("X-Forwarded-Host")
 			forwardedUri := strings.Split(r.Header.Get("X-Forwarded-Uri"), "?")
